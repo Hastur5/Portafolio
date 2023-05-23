e114aa2392710806acd0e4a29d4342dd361cc525
Hastur5 <mgasca12@gmail.com>
2023-05-23 02:42:28 -0600

    se añade texto en la carta y butón

--- Encriptador/index.js
+++ Encriptador/index.js
@@ -1,15 +1,30 @@
-let input = document.querySelector("input");
-let imagen = document.querySelector("img");
-let texto = document.querySelector(".texto");
-let encriptar = document.querySelector(".encriptar");
+const input = document.querySelector("input");
+const caja = document.querySelector(".caja");
+const imagen = document.querySelector("img");
+const texto = document.querySelector(".texto");
+const encriptar = document
+  .querySelector(".encriptar")
+  .addEventListener("click", (evt) => {
+    console.log(input.value);
+    imagen.classList.add("hide");
+    texto.classList.add("hide");
+    mostrarTexto();
+  });
+
 let desencriptar = document
   .querySelector(".desencriptar")
   .addEventListener("click", () => {
     console.log("funciono");
   });
 
-encriptar.addEventListener("click", (evt) => {
-  console.log(input.value);
-  imagen.classList.add("hide");
-  texto.classList.add("hide");
-});
+let mostrarTexto = () => {
+  let texto = document.createElement("h3");
+  texto.innerHTML = `<div class="card">
+                        <p class="resultado"> ${input.value}</p>
+                        <button class="button-copiar">Copiar texto<button/>
+                      </div>`;
+  caja.append(texto);
+  if(){
+    
+  }
+};
