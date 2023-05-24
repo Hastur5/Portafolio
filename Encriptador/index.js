eb1ae7dede78d1c2af720c8c1777e7b4dd7afadc
Hastur5 <mgasca12@gmail.com>
2023-05-23 20:21:59 -0600

    feat: se deja funcionando el botón de encriptar, queda pendiente desencriptar y copiar texto

--- Encriptador/index.js
+++ Encriptador/index.js
@@ -3,6 +3,11 @@ const caja = document.querySelector(".caja");
 const imagen = document.querySelector("img");
 const texto = document.querySelector(".texto");
 let nuevoTexto = "";
+let desencriptar = document
+  .querySelector(".desencriptar")
+  .addEventListener("click", () => {
+    console.log("funciono");
+  });
 const encriptar = document
   .querySelector(".encriptar")
   .addEventListener("click", (evt) => {
@@ -12,16 +17,19 @@ const encriptar = document
     mostrarTexto();
   });
 
-let desencriptar = document
-  .querySelector(".desencriptar")
-  .addEventListener("click", () => {
-    console.log("funciono");
-  });
-
 let mostrarTexto = () => {
+  eliminarTextoAnterior();
   for (let i = 0; i < input.value.length; i++) {
     if (input.value[i] === "a") {
       nuevoTexto += "ai";
+    } else if (input.value[i] === "e") {
+      nuevoTexto += "enter";
+    } else if (input.value[i] === "i") {
+      nuevoTexto += "imes";
+    } else if (input.value[i] === "o") {
+      nuevoTexto += "ober";
+    } else if (input.value[i] === "u") {
+      nuevoTexto += "ufat";
     } else {
       nuevoTexto += input.value[i];
     }
@@ -29,7 +37,18 @@ let mostrarTexto = () => {
   let texto = document.createElement("h3");
   texto.innerHTML = `<div class="card">
                         <p class="resultado"> ${nuevoTexto}</p>
-                        <button class="button-copiar">Copiar texto<button/>
+                        <button class="copiar">Copiar texto<button/>
                       </div>`;
   caja.append(texto);
+  nuevoTexto = "";
+  const copiarBoton = texto.querySelector(".copiar");
+  copiarBoton.addEventListener("click", (evt) => {
+    
+  });
+};
+let eliminarTextoAnterior = () => {
+  const resultadoAnterior = caja.querySelector("h3");
+  if (resultadoAnterior) {
+    caja.removeChild(resultadoAnterior);
+  }
 };
