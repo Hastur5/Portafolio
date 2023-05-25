fdbe70203c128aa9a9949c2460c80837e39e29f8
Hastur5 <mgasca12@gmail.com>
2023-05-25 00:54:24 -0600

    se agrega botón de desencriptado funcionando

--- Encriptador/index.js
+++ Encriptador/index.js
@@ -3,11 +3,6 @@ const caja = document.querySelector(".caja");
 const imagen = document.querySelector("img");
 const texto = document.querySelector(".texto");
 let nuevoTexto = "";
-let desencriptar = document
-  .querySelector(".desencriptar")
-  .addEventListener("click", () => {
-    console.log("funciono");
-  });
 const encriptar = document
   .querySelector(".encriptar")
   .addEventListener("click", (evt) => {
@@ -15,6 +10,13 @@ const encriptar = document
     imagen.classList.add("hide");
     texto.classList.add("hide");
     mostrarTexto();
+  });
+const desencriptar = document
+  .querySelector(".desencriptar")
+  .addEventListener("click", () => {
+    imagen.classList.add("hide");
+    texto.classList.add("hide");
+    mostrarTextoDes();
   });
 
 let mostrarTexto = () => {
@@ -36,16 +38,73 @@ let mostrarTexto = () => {
   }
   let texto = document.createElement("h3");
   texto.innerHTML = `<div class="card">
-                        <p class="resultado"> ${nuevoTexto}</p>
-                        <button class="copiar">Copiar texto<button/>
-                      </div>`;
+  <p class="resultado"> ${nuevoTexto}</p>
+  <button class="copiar">Copiar texto<button/>
+  </div>`;
   caja.append(texto);
-  nuevoTexto = "";
   const copiarBoton = texto.querySelector(".copiar");
   copiarBoton.addEventListener("click", (evt) => {
-    
+    console.log("click");
   });
+  nuevoTexto = "";
 };
+
+let mostrarTextoDes = () => {
+  eliminarTextoAnterior();
+  for (let i = 0; i < input.value.length; i++) {
+    if (input.value[i] === "a" && input.value[i + 1] === "i") {
+      nuevoTexto += "a";
+      i++; // Avanza un índice adicional para saltar la siguiente letra "i"
+    } else if (
+      input.value[i] === "e" &&
+      input.value[i + 1] === "n" &&
+      input.value[i + 2] === "t" &&
+      input.value[i + 3] === "e" &&
+      input.value[i + 4] === "r"
+    ) {
+      nuevoTexto += "e";
+      i += 4; // Avanza cuatro índices adicionales para saltar las letras restantes de "enter"
+    } else if (
+      input.value[i] === "i" &&
+      input.value[i + 1] === "m" &&
+      input.value[i + 2] === "e" &&
+      input.value[i + 3] === "s"
+    ) {
+      nuevoTexto += "i";
+      i += 3; // Avanza tres índices adicionales para saltar las letras restantes de "imes"
+    } else if (
+      input.value[i] === "o" &&
+      input.value[i + 1] === "b" &&
+      input.value[i + 2] === "e" &&
+      input.value[i + 3] === "r"
+    ) {
+      nuevoTexto += "o";
+      i += 3; // Avanza tres índices adicionales para saltar las letras restantes de "ober"
+    } else if (
+      input.value[i] === "u" &&
+      input.value[i + 1] === "f" &&
+      input.value[i + 2] === "a" &&
+      input.value[i + 3] === "t"
+    ) {
+      nuevoTexto += "u";
+      i += 3; // Avanza tres índices adicionales para saltar las letras restantes de "ufat"
+    } else {
+      nuevoTexto += input.value[i];
+    }
+  }
+  let texto = document.createElement("h3");
+  texto.innerHTML = `<div class="card">
+  <p class="resultado"> ${nuevoTexto}</p>
+  <button class="copiar">Copiar texto<button/>
+  </div>`;
+  caja.append(texto);
+  const copiarBoton = texto.querySelector(".copiar");
+  copiarBoton.addEventListener("click", (evt) => {
+    console.log("click");
+  });
+  nuevoTexto = "";
+};
+
 let eliminarTextoAnterior = () => {
   const resultadoAnterior = caja.querySelector("h3");
   if (resultadoAnterior) {
