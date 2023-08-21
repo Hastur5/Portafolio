5763a6515535d45561fa7859e4fc5b86135521c7
Hastur5 <mgasca12@gmail.com>
2023-08-21 17:54:09 -0600

    se pueden agregar cursos al carrito

--- Carrito-Compras/app.js
+++ Carrito-Compras/app.js
@@ -2,6 +2,7 @@ const carrito = document.querySelector("#carrito");
 const contenedorCarrito = document.querySelector("#lista-carrito tbody");
 const vaciarCarritoBtn = document.querySelector("#vaciar-carrito");
 const listaCursos = document.querySelector("#lista-cursos");
+let articulosCarrito = [];
 
 cargarEventlisteners();
 function cargarEventlisteners() {
@@ -11,6 +12,34 @@ function cargarEventlisteners() {
 function agregarCurso(e) {
   e.preventDefault();
   if (e.target.classList.contains("agregar-carrito")) {
-    console.log("se agregó al carrito");
+    const cursoSeleccionado = e.target.parentElement.parentElement;
+    leerDatosCurso(cursoSeleccionado);
+    carritoHTML(articulosCarrito);
+    console.log(cursoSeleccionado);
   }
+}
+
+function leerDatosCurso(curso) {
+  const infoCurso = {
+    imagen: curso.querySelector("img").src,
+    titulo: curso.querySelector("h4").textContent,
+    precio: curso.querySelector("span").textContent,
+    id: curso.querySelector("a").getAttribute("data-id"),
+    cantidad: 1,
+  };
+  articulosCarrito = [...articulosCarrito, infoCurso];
+
+  console.log(articulosCarrito);
+}
+
+function carritoHTML() {
+  articulosCarrito.forEach((curso) => {
+    const row = document.createElement("tr");
+    row.innerHTML = `
+    <td>
+        ${curso.titulo}
+    </td>
+    `;
+    contenedorCarrito.appendChild(row);
+  });
 }
