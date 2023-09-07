c2e6f1db3b3457146f035958c79c94d61c114de0
Hastur5 <mgasca12@gmail.com>
2023-09-07 13:18:41 -0600

    se inicia con la función de registrar a los usarios

--- Veterinaria/backend/controllers/veterinario.controller.js
+++ Veterinaria/backend/controllers/veterinario.controller.js
@@ -1,7 +1,20 @@
-const registrar = (req, res) => {
-  const { nombre, email, password } = req.body;
+import Veterinario from "../models/veterinarioModel.js";
 
-  res.json({ msg: "Registrando a Carmen..." });
+const registrar = async (req, res) => {
+  // const { nombre, email, password } = req.body;
+  try {
+    // Guardar Vet
+    const veterinario = new Veterinario(req.body);
+    const veterinarioGuardado = await veterinario.save();
+
+    //El await detiene la ejecución hasta que se guarde esa línea. Para que la siguiente no se ejecute sin que la info haya sido guardada.
+
+    res.json({
+      msg: veterinarioGuardado,
+    });
+  } catch (error) {
+    console.log(error);
+  }
 };
 
 const perfil = (req, res) => {
