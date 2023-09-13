734459f28f2194344c05e0d8ed650cb5e1763022
Hastur5 <mgasca12@gmail.com>
2023-09-13 01:15:03 -0600

    se agrega confirmación de token

--- Veterinaria/backend/controllers/veterinario.controller.js
+++ Veterinaria/backend/controllers/veterinario.controller.js
@@ -31,9 +31,25 @@ const perfil = (req, res) => {
   res.json({ msg: "Mostrando perfil" });
 };
 
-const confirmar = (req, res) => {
-  console.log(req.params.token); //params se usa para leer la url.
-  res.json({ msg: "Confirmando cuenta..." });
+const confirmar = async (req, res) => {
+  const { token } = req.params;
+  const usuarioConfirmar = await Veterinario.findOne({ token });
+
+  if (!usuarioConfirmar) {
+    const error = new Error("Token no válido");
+    return res.status(400).json({ msg: error.message });
+  }
+
+  try {
+    usuarioConfirmar.token = null;
+    usuarioConfirmar.confirmado = true;
+
+    await usuarioConfirmar.save()
+
+    res.json({ msg: "Usuario confirmado correctamente." });
+  } catch (error) {
+    console.log(error);
+  }
 };
 
 export { registrar, perfil, confirmar };
